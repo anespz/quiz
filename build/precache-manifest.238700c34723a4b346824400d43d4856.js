3feac75cbf16bb7efba2872e3993cb9dd56181e1
ane <ane.espeseth@gmail.com>
2020-01-22 05:35:19 +0000

    build

---
+++ build/precache-manifest.238700c34723a4b346824400d43d4856.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f2bb1cfd44b79bf8485172a086c053",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "cf5425ab7538fbcac5ca",
+    "url": "/quiz/static/css/main.7eca77df.chunk.css"
+  },
+  {
+    "revision": "88c6e7669eb550efcac3",
+    "url": "/quiz/static/js/2.19281a09.chunk.js"
+  },
+  {
+    "revision": "edf65f475b9a1bfa4fd32a617c3e87d9",
+    "url": "/quiz/static/js/2.19281a09.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf5425ab7538fbcac5ca",
+    "url": "/quiz/static/js/main.6c6ebd4f.chunk.js"
+  },
+  {
+    "revision": "c7bedca3bd359de5a688",
+    "url": "/quiz/static/js/runtime-main.d08f2342.js"
+  }
+]);
